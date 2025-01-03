f3ee24768e10f04d4f9dde1e36fa00e980c03795
namnh198 <me@namhoainguyen.com>
2025-01-03 13:59:20 +0700

    chore: remove end slash sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -26,29 +26,29 @@ const getSitemap = async (site: URL | undefined) => {
   const posts = await getPosts();
   const tags = await getTags();
 
-  return `  
-    <?xml version="1.0" encoding="UTF-8"?>  
-    <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="https://www.w3.org/1999/xhtml" xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="https://www.google.com/schemas/sitemap-image/1.1" xmlns:video="https://www.google.com/schemas/sitemap-video/1.1">  
+  return `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="https://www.w3.org/1999/xhtml" xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="https://www.google.com/schemas/sitemap-image/1.1" xmlns:video="https://www.google.com/schemas/sitemap-video/1.1">
       ${getSiteMapItem({ loc: site?.toString() })}
-      ${getSiteMapItem({ loc: `${site}/tags/` })}
-      ${getSiteMapItem({ loc: `${site}/notes/` })}
-      ${getSiteMapItem({ loc: `${site}/bookmarks/` })}
-      ${getSiteMapItem({ loc: `${site}/support-me/` })}
-      ${getSiteMapItem({ loc: `${site}/privacy/` })}
+      ${getSiteMapItem({ loc: `${site}tags` })}
+      ${getSiteMapItem({ loc: `${site}notes` })}
+      ${getSiteMapItem({ loc: `${site}bookmarks` })}
+      ${getSiteMapItem({ loc: `${site}support-me` })}
+      ${getSiteMapItem({ loc: `${site}privacy` })}
       ${tags
         .map(tag => {
-          const loc = `${site}tags/${tag.slug}/`;
+          const loc = `${site}tags/${tag.slug}`;
           return getSiteMapItem({ loc });
         })
         .join('\n')}
       ${posts
         .map(post => {
-          const loc = `${site}notes/${post.slug}/`;
+          const loc = `${site}notes/${post.slug}`;
           const lastMod = post.updateDate;
           return getSiteMapItem({ loc, lastMod });
         })
-        .join('\n')}  
-    </urlset>  
+        .join('\n')}
+    </urlset>
   `.trim();
 };
 
